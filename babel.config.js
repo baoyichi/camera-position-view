f617118b2cc1dbfaad5753e85cbca11b2b796f5a
Leo <kb1524@163.com>
2022-09-08 14:05:20 +0800

    项目初始化

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,10 @@
+const consoleConfig = process.env.NODE_ENV;
+const pluginConfit = [];
+if (consoleConfig === 'production') {
+  pluginConfit.push('transform-remove-console')
+}
+
 module.exports = {
   presets: ["@vue/cli-plugin-babel/preset"],
+  plugins: pluginConfit
 };
--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,7 @@ function resolve(dir) {
 
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/production-sub-path/'
+    ? '/projectName/'
     : '/',
   // 在npm run build 或 yarn build 时 ，生成文件的目录名称（要和baseUrl的生产环境路径一致）（默认dist）
   outputDir: 'dist',
