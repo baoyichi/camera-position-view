edac8c596556563c4f05a0df49cffa7be7a06d92
Leo <kb1524@163.com>
2022-09-08 15:08:33 +0800

    引入echarts

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,14 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import ElementUI from 'element-ui';
+Vue.use(ElementUI);
+
+import * as echarts from 'echarts';
+Vue.prototype.$echarts = echarts;
 
 import 'element-ui/lib/theme-chalk/index.css';
 import '@/assets/style/common.scss';
 
-Vue.use(ElementUI);
 Vue.config.productionTip = false;
 
 new Vue({
